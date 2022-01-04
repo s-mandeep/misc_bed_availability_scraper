5e3f4143dcbbbbc747f273426fd69aafe580809d
Mandeep Singh <mandeepsrr@gmail.com>
2022-01-04 17:28:54 +0530

    fixed scraping for Bengaluru Hosp. Data

--- scraper.py
+++ scraper.py
@@ -22,19 +22,51 @@ if __name__=='__main__':
       options.add_argument('--disable-gpu');
       options.add_argument("--headless")
       options.add_argument("--window-size=1366,768")
+      options.add_experimental_option("excludeSwitches", ["enable-automation"])
+      options.add_experimental_option('useAutomationExtension', False)
       driver=webdriver.Chrome(chrome_options=options)  
       driver.get('https://apps.bbmpgov.in/Covid19/en/bedstatus.php')
-      driver.get('https://www.powerbi.com/view?r=eyJrIjoiOTcyM2JkNTQtYzA5ZS00MWI4LWIxN2UtZjY1NjFhYmFjZDBjIiwidCI6ImQ1ZmE3M2I0LTE1MzgtNGRjZi1hZGIwLTA3NGEzNzg4MmRkNiJ9')
-      driver.get('20.186.65.100/view?r=eyJrIjoiOTcyM2JkNTQtYzA5ZS00MWI4LWIxN2UtZjY1NjFhYmFjZDBjIiwidCI6ImQ1ZmE3M2I0LTE1MzgtNGRjZi1hZGIwLTA3NGEzNzg4MmRkNiJ9')
       time.sleep(10)
-      date=datetime.datetime.now();date_str=date.strftime('%d_%m_%Y')
-      if not os.path.exists('images/'+date_str+'.png'):
-        driver.save_screenshot('images/'+date_str+'.png')
-        img=Image.open('images/'+date_str+'.png')
-        img.save('images/'+date_str+'.webp')
-        print('saved screenshot of bengaluru beds availability dashboard to %s' %('images/'+date_str+'.webp'))
+      WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,"iframe")))
+
+      parent = driver.find_element(By.TAG_NAME,'body')
+      v = parent.find_element(By.CLASS_NAME,'visualContainerHost')
+      v =  v.find_elements(By.CLASS_NAME,'multiRowCard')
+      results = []
+      for e in v:
+        cards = e.find_elements(By.CLASS_NAME,'card')
+        for c in cards:
+          label = c.get_attribute('aria-label')
+          label = label.replace('.','')
+          res = [int(i) for i in label.split() if i.isdigit()]
+          results.append([res[1],res[3]])
+
+      print(results)
+      general_available = results[0][1]
+      general_admitted = results[0][0]
+
+      hdu_available = results[1][1]
+      hdu_admitted = results[1][0]
+
+      icu_available = results[2][1]
+      icu_admitted = results[2][0]
+
+      ventilator_available = results[3][1]
+      ventilator_admitted = results[3][0]
+
+
+      a=open('data.bengaluru.csv');r=csv.reader(a);info=[i for i in r];a.close()
+      dates=list(set([i[0] for i in info[1:]]));dates.sort()
+      
+      if date_str in dates: 
+        # ~ dont_update_data_csv=True
+        print('----------\n\nData for %s already exists in csv!!\nOnly printing, not modifying csv!!\n\n----------\n\n' %(date_str))
       else:
-        print('Image: %s already existed. Skipping!!' %('images/'+date_str+'.png'))
+        #write to file
+        info=', '.join((date_str,str(general_available),str(general_admitted),str(hdu_available),str(hdu_admitted),str(icu_available),str(icu_admitted),str(ventilator_available),str(ventilator_admitted)))        
+        a=open('data.bengaluru.csv','a');a.write(info+'\n');a.close()
+        print('Appended to data.bengaluru.csv: '+info) 
+
     elif city=='gbn':
  #check if data for given date already exists in csv. Update only if data doesn't exist
       a=open('data.gbn.csv');r=csv.reader(a);info=[i for i in r];a.close()
